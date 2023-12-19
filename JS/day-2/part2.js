d6506b4f99c228bf377a60c7b0b623cb47bab459
JonesMertz <jonasmertz@gmail.com>
2023-12-20 00:57:58 +0100

    Day 2 - part 2

---
+++ JS/day-2/part2.js
@@ -0,0 +1,60 @@
+const { prototype } = require('events');
+const fs = require('fs');
+const rawGames = fs.readFileSync(__dirname + '/input-values.txt').toString().split('\n')
+//const gameIDs = rawGames.split(":").map(x => parseInt(x[0].replace("Game ", "")));
+//const gameParsed = rawGames.split(":").map(x => x[1].split(";"));
+
+
+String.prototype.capitalize = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+let sum = 0;
+for (let i = 0; i < rawGames.length; i++) {
+    let gameCounter = {
+        blue: 0,
+        minimumRequiredBlue: 0,
+        red: 0,
+        minimumRequiredRed: 0,
+        green: 0,
+        minimumRequiredGreen: 0,
+        wasInvalid: false,
+        addToCount: function (color, count) {
+            this[color.toLowerCase()] += count;
+        },
+        changeCount: function (color, count) {
+            this["minimumRequired" + color.capitalize()] = Math.max(count, this["minimumRequired" + color.capitalize()]);
+        },
+        getProductOfMinimumRequired: function () {
+            return this.minimumRequiredBlue * this.minimumRequiredGreen * this.minimumRequiredRed;
+        },
+        isValidSubset: function () {
+            const isInvalid = this.red > 12 || this.green > 13 || this.blue > 14;
+            if (isInvalid) {
+                this.wasInvalid = true;
+            }
+            return isInvalid;
+        },
+        resetCount: function () {
+            this.blue = 0;
+            this.red = 0;
+            this.green = 0;
+        }
+    }
+    let [gameID, game] = rawGames[i].split(":")
+    gameID = parseInt(gameID.replace("Game ", ""));
+    subsets = game.split(";");
+    subsets.forEach((subset) => {
+        const cubes = subset.split(",");
+        cubes.forEach((cube) => {
+            const [count, color] = cube.trim().split(" ");
+            gameCounter.addToCount(color, parseInt(count));
+            gameCounter.changeCount(color, parseInt(count));
+        })
+        gameCounter.isValidSubset();
+        gameCounter.resetCount();
+    })
+    sum += gameCounter.getProductOfMinimumRequired();
+}
+console.log(sum);
+
